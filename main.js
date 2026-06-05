324b3a03aa1026668984522f91df26dffb330ac0
Marco <colombomarco07@gmail.com>
2026-06-05 14:46:58 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let num1 = 10
+let num2 = 15
+let num3 = 20
+let num4 = 25
+let num5 = 30
+
+console.log(num1, num2, num3, num4, num5);
+
+let somma = num1 + num2 + num3 + num4 + num5
+
+let media = somma / 5
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
